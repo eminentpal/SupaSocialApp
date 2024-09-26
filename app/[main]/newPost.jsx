e1b4b930782ebb3d417de3ddfda131c5b95158de
eminentpal <eziechukwudi@gmail.com>
2024-09-26 23:37:52 +0100

    New post style sheet completed

--- app/[main]/newPost.jsx
+++ app/[main]/newPost.jsx
@@ -40,6 +40,61 @@ const styles = StyleSheet.create({
   fontWeight: theme.fonts.semibold,
   color: theme.colors.text
  },
- ava
+ avatar:{
+  height: hp(6.5),
+  width:hp(6.5),
+  borderRadius: theme.radius.xl,
+  borderCurve:'continuous',
+  borderWidth: 1,
+  borderColor: 'rgba(0.0.0.0.1',
+
+ },
+ publicText:{
+  fonSize: hp(1.7),
+  fontWeight:theme.fonts.medium,
+  color:theme.colors.textLight
+ },
+ textEditor:{
+  //marginTop:10
+ },
+ media:{
+  flexDirection: 'row',
+  justifyContent:'space-between',
+  alignItems: 'center',
+  borderWidth: 1.5,
+  padding:12,
+  paddingHorizontal:18,
+  borderColor: theme.radius.xl,
+  borderCurve:'continuous',
+  borderColor: theme.colors.gray,
+ },
+ mediaIcons:{
+  flexDirection: 'row',
+  alignItems:'center',
+  gap:15
+ },
+ addImageText:{
+  fonSize:hp(1.9),
+  fontWeight: theme.fonts.semibold,
+  color:theme.colors.text,
+ },
+ imageIcon:{
+  borderRadius: theme.radius.md,
+ },
+ file:{
+  height:hp(30),
+  width:'100%',
+  borderRadius: theme.radius.xl,
+  overflow:'hidden',
+  borderCurve:'continuous'
+ },
+ video:{
+
+ },
+ closeIcon:{
+  position: 'absolute',
+  top: 10,
+  right: 10,
+ }
 
 })
