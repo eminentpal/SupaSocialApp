59570399ba0bf0312fad30ddd455ce57c54a3a15
eminentpal <eziechukwudi@gmail.com>
2024-11-10 10:01:48 +0100

    App completed

--- app/(main)/home.jsx
+++ app/(main)/home.jsx
@@ -148,7 +148,10 @@ const Home = () => {
        <View style={styles.header}>
          <Text style={styles.title} >LinkUp</Text>
          <View style={styles.icons}>
-           <Pressable onPress={()=> router.push('notifications')}>
+           <Pressable onPress={()=> {
+            setnotificationCount(0)
+            router.push('notifications')
+            }}>
               <Icon  name="heart" size={hp(3.2)} strokeWidth={2} color={theme.colors.text} />
              {
                notificationCount > 0 && (
